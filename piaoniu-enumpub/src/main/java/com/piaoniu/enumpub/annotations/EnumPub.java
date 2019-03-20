f16baf3d1223a60e40c5ccf4bc9f168406731de1
yann <yang.wang@ipiaoniu.com>
2019-03-20 13:21:22 +0800

    fix

--- piaoniu-enumpub/src/main/java/com/piaoniu/enumpub/annotations/EnumPub.java
+++ piaoniu-enumpub/src/main/java/com/piaoniu/enumpub/annotations/EnumPub.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target(value = ElementType.TYPE)
-@Retention(RetentionPolicy.SOURCE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface EnumPub {
 
     String value() default "value";
