69396d94e1bbfac2dba07417a49c55496f8f9120
yann <yang.wang@ipiaoniu.com>
2019-03-20 16:18:44 +0800

    fix service

--- piaoniu-enumpub/src/main/java/com/piaoniu/enumpub/service/PublishManager.java
+++ piaoniu-enumpub/src/main/java/com/piaoniu/enumpub/service/PublishManager.java
@@ -1,9 +1,13 @@
 package com.piaoniu.enumpub.service;
 
+import com.google.common.collect.Lists;
 import com.piaoniu.enumpub.annotations.EnumPub;
+import com.piaoniu.enumpub.domain.TypeValue;
 import com.piaoniu.enumpub.util.ScanUtil;
 
+import java.lang.reflect.Method;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public abstract class PublishManager {
 
@@ -23,5 +27,33 @@ public abstract class PublishManager {
                             });
                 }
         );
+    }
+
+    public List<String> publishEnumNames() {
+        return getSimpleNames();
+    }
+
+    public List<TypeValue> publishEnum(String enumName) {
+        try{
+            Class enumClass = getEnumClass(enumName);
+            Object[] objects = enumClass.getEnumConstants();
+            Method getValue = enumClass.getMethod("getValue");
+            Method getDesc = enumClass.getMethod("getDesc");
+            return Lists.newArrayList(objects).stream().map(o -> {
+                        try{
+                            return TypeValue.of(Integer.valueOf(getValue.invoke(o).toString()),getDesc.invoke(o).toString());
+                        }catch (Exception e){
+                            return null;
+                        }
+                    }
+            ).collect(Collectors.toList());
+        } catch (Exception e){
+            return Lists.newArrayList();
+        }
+    }
+
+    private Class getEnumClass(String name) throws ClassNotFoundException{
+        String realName = getRealName(name);
+        return Class.forName(realName);
     }
 }
