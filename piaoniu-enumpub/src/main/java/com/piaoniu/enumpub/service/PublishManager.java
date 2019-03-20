e46423a52a955e0f97cba52154df4864c56d5e74
yann <yang.wang@ipiaoniu.com>
2019-03-20 14:23:06 +0800

    fix postProcessor

--- piaoniu-enumpub/src/main/java/com/piaoniu/enumpub/service/PublishManager.java
+++ piaoniu-enumpub/src/main/java/com/piaoniu/enumpub/service/PublishManager.java
@@ -1,12 +1,27 @@
 package com.piaoniu.enumpub.service;
 
+import com.piaoniu.enumpub.annotations.EnumPub;
+import com.piaoniu.enumpub.util.ScanUtil;
+
 import java.util.List;
 
-public interface PublishManager {
+public abstract class PublishManager {
 
-    void saveNames(String name,String simpleName);
+    abstract void saveNames(String name,String simpleName);
 
-    String getRealName(String simpleName);
+    abstract String getRealName(String simpleName);
 
-    List<String> getSimpleNames();
+    abstract List<String> getSimpleNames();
+
+    public void init(List<String> packageNames){
+        packageNames.forEach(
+                p -> {
+                    ScanUtil.getClasses(p).stream()
+                            .filter(c -> c.getAnnotation(EnumPub.class)!=null)
+                            .forEach(c -> {
+                                saveNames(c.getSimpleName(),c.getName());
+                            });
+                }
+        );
+    }
 }
